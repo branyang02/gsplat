a8caab3fb7693bc6ac02fd7e4a7193873e5b600f
Brandon Yang <jqm9ba@virginia.edu>
2024-06-26 15:19:13 -0400

    changed name from app_opt to sam_opt module

--- examples/utils.py
+++ examples/utils.py
@@ -46,6 +46,73 @@ class CameraOptModule(torch.nn.Module):
         return torch.matmul(camtoworlds, transform)
 
 
+class SAMOptModule(torch.nn.Module):
+    def __init__(
+        self,
+        n: int,
+        feature_dim: int,
+        embed_dim: int = 16,
+        sh_degree: int = 3,
+        mlp_width: int = 64,
+        mlp_depth: int = 2,
+        output_dim: int = 3,  ##### added output_dim
+    ):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.sh_degree = sh_degree
+        self.embeds = torch.nn.Embedding(n, embed_dim)
+        layers = []
+        layers.append(
+            torch.nn.Linear(embed_dim + feature_dim + (sh_degree + 1) ** 2, mlp_width)
+        )
+        layers.append(torch.nn.ReLU(inplace=True))
+        for _ in range(mlp_depth - 1):
+            layers.append(torch.nn.Linear(mlp_width, mlp_width))
+            layers.append(torch.nn.ReLU(inplace=True))
+        layers.append(
+            torch.nn.Linear(mlp_width, output_dim)
+        )  ##### changed from 3 to output_dim
+        self.color_head = torch.nn.Sequential(*layers)
+
+    def forward(
+        self, features: Tensor, embed_ids: Tensor, dirs: Tensor, sh_degree: int
+    ) -> Tensor:
+        """Adjust appearance based on embeddings.
+
+        Args:
+            features: (N, feature_dim)
+            embed_ids: (C,)
+            dirs: (C, N, 3)
+
+        Returns:
+            colors: (C, N, 3)
+        """
+        from gsplat.cuda._torch_impl import _eval_sh_bases_fast
+
+        C, N = dirs.shape[:2]
+        # Camera embeddings
+        if embed_ids is None:
+            embeds = torch.zeros(C, self.embed_dim, device=features.device)
+        else:
+            embeds = self.embeds(embed_ids)  # [C, D2]
+        embeds = embeds[:, None, :].expand(-1, N, -1)  # [C, N, D2]
+        # GS features
+        features = features[None, :, :].expand(C, -1, -1)  # [C, N, D1]
+        # View directions
+        dirs = F.normalize(dirs, dim=-1)  # [C, N, 3]
+        num_bases_to_use = (sh_degree + 1) ** 2
+        num_bases = (self.sh_degree + 1) ** 2
+        sh_bases = torch.zeros(C, N, num_bases, device=features.device)  # [C, N, K]
+        sh_bases[:, :, :num_bases_to_use] = _eval_sh_bases_fast(num_bases_to_use, dirs)
+        # Get colors
+        if self.embed_dim > 0:
+            h = torch.cat([embeds, features, sh_bases], dim=-1)  # [C, N, D1 + D2 + K]
+        else:
+            h = torch.cat([features, sh_bases], dim=-1)
+        colors = self.color_head(h)
+        return colors
+
+
 ##### TODO: Change the name of this class
 class AppearanceOptModule(torch.nn.Module):
     """Appearance optimization module."""
