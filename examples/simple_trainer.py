51e421bb3dbd9521e7604e4f0f1d7295570c3207
Brandon Yang <jqm9ba@virginia.edu>
2024-07-03 16:26:06 -0400

    added kwargs for viewer_fn

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -939,7 +939,7 @@ class Runner:
 
     @torch.no_grad()
     def _viewer_render_fn(
-        self, camera_state: nerfview.CameraState, img_wh: Tuple[int, int]
+        self, camera_state: nerfview.CameraState, img_wh: Tuple[int, int], **kwargs
     ):
         """Callable function for the viewer."""
         W, H = img_wh
@@ -955,6 +955,7 @@ class Runner:
             height=H,
             sh_degree=self.cfg.sh_degree,  # active all SH degrees
             radius_clip=3.0,  # skip GSs that have small image radius (in pixels)
+            **kwargs,
         )  # [1, H, W, 3]
         return render_colors[0].cpu().numpy()
 
